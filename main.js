f1b11715b1ba77b623cc9d863be369c36752464d
Bomyee Woo <bomyeew@gmail.com>
2020-08-02 10:08:49 -0400

    Add color pick animation and fix layout

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    console.log($('#numberBox1').text());
-})
+    //Click color
+    $('.color').click(function() {
+        // Animation to boxes
+        for (i = 0; i < $(this).text().length; i++) {
+            $('#colorBox1').animate({bottom: '300px'}, 1000).animate({bottom: '0px'}, 1000).animate({right: '300px'}, 1000).animate({right: '0px'}, 1000);
+            $('#colorBox2').animate({bottom: '300px'}, 1000).animate({bottom: '0px'}, 1000).animate({left: '300px'}, 1000).animate({left: '0px'}, 1000);
+            $('#colorBox3').animate({top: '300px'}, 1000).animate({top: '0px'}, 1000).animate({right: '300px'}, 1000).animate({right: '0px'}, 1000);
+            $('#colorBox4').animate({top: '300px'}, 1000).animate({top: '0px'}, 1000).animate({left: '300px'}, 1000).animate({left: '0px'}, 1000);
+        }
+
+        setTimeout(function() {
+            $('#fortune-container1').hide();
+            $('#fortune-container2').show();
+        }, $(this).text().length * 2000);
+    });
+});
